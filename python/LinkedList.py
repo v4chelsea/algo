6193d6d7951e08e548b5228e85f283c804132f4c
marin <marin@studiolab.ai>
2023-10-02 23:48:45 +0900

    feat : Add Python Code of LinkedList

--- python/LinkedList.py
+++ python/LinkedList.py
@@ -9,3 +9,99 @@ class LinkedList:
         self.head = None
         self.count = 0
 
+
+    def printAll(self):
+        text = "["
+        currentNode = self.head
+
+        while (currentNode != None):
+            text += str(currentNode.data)
+            currentNode = currentNode.next
+
+            if (currentNode != None):
+                text += ", "
+            
+        text += "]"
+        print(text)
+            
+
+    def insertAt(self, index, data):
+        if (index > self.count) or (index < 0):
+            IndexError("범위를 넘어섰습니다")
+
+        newNode = Node(data)
+
+        if index == 0:
+            newNode.next = self.head
+            self.head = newNode
+        else:
+            currentNode = self.head
+
+            for _ in range(index - 1):
+                currentNode = currentNode.next
+
+            newNode.next = currentNode.next
+            currentNode.next = newNode
+
+        self.count += 1
+
+    def insertLast(self, data):
+        newNode = Node(data)
+        currentNode = self.head
+
+        for _ in range(self.count - 1):
+            currentNode = currentNode.next
+
+        # while (currentNode != None):
+        #     currentNode = currentNode.next
+
+        currentNode.next = newNode
+        self.count += 1
+
+    def clear(self):
+        self.head = None
+        self.count = 0
+
+
+    def deleteAt(self, index):
+        if (index > self.count) or (index < 0):
+            IndexError("범위를 넘어섰습니다")
+
+        if index == (self.count - 1):
+            self.deleteLast()
+        else:
+            if (index == 0) and (self.count > 1):
+                self.head = self.head.next
+            elif (index == 0) and (self.count == 1):
+                self.head = None
+            else:
+                currentNode = self.head
+
+                for _ in range(index - 1):
+                    currentNode = currentNode.next
+
+                currentNode.next = currentNode.next.next
+
+            self.count -= 1
+
+
+    def deleteLast(self):
+        currentNode = self.head
+
+        for _ in range(self.count - 2):
+            currentNode = currentNode.next
+
+        currentNode.next = None
+        self.count -= 1
+
+    def getNodeAt(self, index):
+        if (index > self.count) or (index < 0):
+            IndexError("범위를 넘어섰습니다")
+
+        currentNode = self.head
+
+        while index != 0:
+            currentNode = currentNode.next
+            index -= 1
+
+        print(currentNode.data)
--- python/test.py
+++ python/test.py
@@ -1,4 +1,4 @@
-from LinkedList import Node
+from LinkedList import Node, LinkedList
 
 node1 = Node(10)
 node2 = Node(3)
@@ -10,3 +10,43 @@ node2.next = node3
 print(node1.data)
 print(node1.next.data)
 print(node1.next.next.data)
+
+list = LinkedList()
+
+list.insertAt(0, 0)
+list.insertAt(1, 1)
+list.insertAt(2, 2)
+list.insertAt(3, 3)
+list.insertAt(4, 4)
+list.insertAt(5, 5)
+list.printAll()
+
+# ====================
+
+list.insertLast(6)
+list.insertLast(7)
+list.insertLast(8)
+list.insertLast(9)
+list.printAll()
+
+# ====================
+
+list.getNodeAt(3)
+list.getNodeAt(8)
+list.getNodeAt(1)
+list.getNodeAt(2)
+
+# ====================
+
+list.deleteAt(4)
+list.printAll()
+
+# ====================
+
+list.deleteLast()
+list.printAll()
+
+# ====================
+
+list.clear()
+list.printAll()
