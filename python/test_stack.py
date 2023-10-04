43ee8222acbc2861ace986d34a288688558dde67
marin <marin@studiolab.ai>
2023-10-04 09:36:36 +0900

    feat : add stack test

---
+++ python/test_stack.py
@@ -0,0 +1,18 @@
+from Stack import Stack
+
+stack = Stack()
+
+stack.push(0)
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+def test_stack():
+    assert stack.pop().data == 5
+    assert stack.pop().data == 4
+    assert stack.pop().data == 3
+    assert stack.pop().data == 2
+    assert stack.pop().data == 1
+    assert stack.pop().data == 0
