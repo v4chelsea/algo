9eb119cc23992665e1b27318e4c2b16ec17765c5
marin <marin@studiolab.ai>
2023-10-07 23:58:07 +0900

    feat : change test stack

--- python/test_stack.py
+++ python/test_stack.py
@@ -8,8 +8,10 @@ stack.push(2)
 stack.push(3)
 stack.push(4)
 stack.push(5)
+stack.push(6)
 
 def test_stack():
+    assert stack.pop().data == 6
     assert stack.pop().data == 5
     assert stack.pop().data == 4
     assert stack.pop().data == 3
